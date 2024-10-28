21fec00c694425f566e4fa3cd3fb0c6cc209c99e
matebonfanti <matebonfanti@gmail.com>
2024-10-28 16:55:09 -0300

    Coletado informações das tabelas de diferentes portos e concatenado em uma tabela so

--- extract.py
+++ extract.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+
 #Função para extrair a tabela de um site, com a URL e o nome da tabela, e retorna um Data Frame com as informações
 def extrair_tabela(url, nome):
         # Fazer a requisição HTTP para obter o conteúdo da página
@@ -10,14 +11,11 @@ def extrair_tabela(url, nome):
     # Analisar o HTML da página
     soup = BeautifulSoup(resposta.content, 'html.parser')
 
-    # Encontrar a tabela desejada (exemplo com id)
-    #tabela = soup.find('table')
-    #tab_exp = tabela[2]
 
     # Encontrar todas as tabelas
     tabelas = soup.find_all('table')
 
-    # Filtrar a tabela desejada
+    # Filtrar a tabela desejada pelo nome passado por parametro
     tabela_desejada = None
     for tabela in tabelas:
         if tabela.find(text=nome):
@@ -38,5 +36,13 @@ def extrair_tabela(url, nome):
         print('Tabela não encontrada')
 
 
-    #Criar um Data Frame
-    return pd.DataFrame(dados_tabela)
+    #cria o Data frame, Remove as 2 primeiras linhas, (titulo e Titulo da Coluna)
+
+    df = pd.DataFrame(dados_tabela)
+    df = df.iloc[2:].reset_index(drop=True)
+    
+
+
+    #Retorna o Data Frame
+
+    return (df)
--- main.py
+++ main.py
@@ -8,11 +8,59 @@ url_santos = 'https://www.portodesantos.com.br/informacoes-operacionais/operacoe
 url_paranagua = 'https://www.appaweb.appa.pr.gov.br/appaweb/pesquisa.aspx?WCI=relLineUpRetroativo'
 
 
+
+
+# Como as 2 fontes de dados são diferentes, crio 2 maneiras de coletar os dados, uma para cada porto. 
+
+######------------------------------- SANTOS -------------------------#
+#Extrai a tabela de Importação do Porto de Santos
 df = extrair_tabela(url_santos, 'GRANEIS SOLIDOS - IMPORTACAO')
+df_santos_import = df[[4, 8, 9]]
+
+#Extrai a tabela de Exportação do Porto de Santos
+df = extrair_tabela(url_santos, 'GRANEIS SOLIDOS - EXPORTACAO')
+df_santos_export = df[[4, 8, 9]]
+
+df_filtrado_import = pd.DataFrame({
+    #'ID': range(1, len(df_santos_import) + 1),
+    'Produto':df_santos_import[8],
+    'Data': df_santos_import[4],
+    'Peso': df_santos_import[9],
+    'Exp/Imp': 'Imp',
+     'Porto': 'Santos' }) 
+
+df_filtrado_export = pd.DataFrame({
+    #'ID': range(1, len(df_santos_export) + 1),
+    'Produto': df_santos_export[8],
+    'Data': df_santos_export[4],
+    'Peso': df_santos_export[9],
+    'Exp/Imp': 'Exp',
+     'Porto': 'Santos' }) 
+#Junta os 2 DF em um só
+df_final_Santos = pd.concat([df_filtrado_export, df_filtrado_import])
+
+######---------------------------------------------------------------------#
+
+######------------------------------- PARANAGUA -------------------------#
+df_paranagua = extrair_tabela(url_paranagua, 'ESPERADOS')
+
+df_paranagua = pd.DataFrame({
+    #'ID': range(1, len(df_santos_import) + 1),
+    'Produto':df_paranagua[11],
+    'Data': df_paranagua[12],
+    'Peso': df_paranagua[15],
+    'Exp/Imp': df_paranagua[8],
+     'Porto': 'Paranagua' }) 
 
 
 
-print(df)
-# Mostrar os dados na tela
-# for linha in dados_tabela:
-#     print('\t'.join(linha))
+df_portos = pd.concat([df_final_Santos, df_paranagua])
+print(df_portos)
+
+
+
+#Problemas encontrados:
+#1 - Como encontrar ou puxar as informações somente com o nome da coluna 
+#2 - Padrozinar as coletas de dados de diferentes sites usando um método simples que funcione em todos, (Por exemplo nesse caso, em santos tinha 1 tabela pra exportação e outra para importação,
+#     e o outro tem uma coluna única que junta essas informações)
+#3 - Remover as linhas indesejadas na hora da coleta de informações e não após ja ter coletado a tabela toda ( Como no caso ele puxa o titulo da tabela e o titulo da coluna e após eu removo)
